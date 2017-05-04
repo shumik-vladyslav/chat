bfd2147f94792bf39b0a1aeb33598bed7c86ca9f
Shumik Vlad <shumik.vladyslav@gmail.com>
2017-05-04 21:24:59 +0300

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,17 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: "AIzaSyBXui6E9WHeIkYe-41Wm1sUFQksGUm-roI",
+  authDomain: "chat-828b8.firebaseapp.com",
+  databaseURL: "https://chat-828b8.firebaseio.com",
+  projectId: "chat-828b8",
+  storageBucket: "chat-828b8.appspot.com",
+  messagingSenderId: "663248235118"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +23,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
